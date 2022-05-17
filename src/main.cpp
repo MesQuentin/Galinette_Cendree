1e877bceaefbacd11488f06489e36e48754dd401
Sujet Test <atiusem@gmail.com>
2022-05-17 08:55:14 +0200

    [p6] p6 Abandonment
    A console game seems to  be way easier to do

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,4 @@
 int main()
 {
     Board board = {9, {{2, 3}}};
-    auto  ctx   = p6::Context{{800, 800, "La Galinette Cendrée"}};
-    ctx.update  = [&]() {
-        draw_board(board, ctx);
-    };
-    ctx.start();
 }
